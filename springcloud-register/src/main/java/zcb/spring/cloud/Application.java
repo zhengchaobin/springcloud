c4c8a0b76eb168a82953458312ee659363dac660
zhengchaobin <zhengchaobin@yunnex.com>
2018-10-09 16:57:21 +0800

    初始化工程

---
+++ springcloud-register/src/main/java/zcb/spring/cloud/Application.java
@@ -0,0 +1,21 @@
+package zcb.spring.cloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author zhengchaobin
+ * Date: 2018/10/9
+ * Description:
+ **/
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+
+}
